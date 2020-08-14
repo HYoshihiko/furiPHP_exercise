e339f192d0f050c7f5f3eb8eb72299de4b78bd5c
HYoshihiko <yoshi429to3@gmail.com>
2020-08-14 17:14:18 +0900

    chapter3 is submitted

---
+++ chap3_2.php
@@ -0,0 +1,20 @@
+<?php
+foreach(range(1, 100) as $int){
+    if($int % 3 == 0){
+        print("アホ ");
+    }else{
+        print($int . " ");
+    }
+}
+
+
+
+/*
+for($i=1; $i<=100; $i++){
+    if($i % 3 == 0){
+        echo("アホ ");
+    }else{
+        echo($i . " ");
+    }
+}
+*/
